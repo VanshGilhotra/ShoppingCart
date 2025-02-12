fc02f98ff5a18ccf96535d86cbe1b2bdd0512f50
VanshGilhotra <vanshgilhotra00992@gmail.com>
2025-02-12 13:03:58 +0530

    Error Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,60 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {FruitsData} from './StoreItems'
+import { FruitsData } from './StoreItems'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [SelectedItem,setSelectedItems] = useState([]);
-  const [Fruit,setFruits] = useState(FruitsData);
+
+  const [Fruit, setFruits] = useState(FruitsData);
 
   const increaseQuantity = (id) => {
     setFruits(Fruit.map(fruit =>
       fruit.id === id ? { ...fruit, quantity: fruit.quantity + 1 } : fruit
     ));
-};
 
-const decreaseQuantity = (id) =>{
+  };
+
+  const decreaseQuantity = (id) => {
     setFruits(Fruit.map(fruit =>
-        fruit.id === id && fruit.quantity > 0 ?{...fruit,quantity :fruit.quantity - 1} : fruit
+      fruit.id === id && fruit.quantity > 0 ? { ...fruit, quantity: fruit.quantity - 1 } : fruit
     ))
-}
+  }
 
+  const [cart, setcartItems] = useState([]);
+
+  const handleCartAddOn = (id) => {
+
+    if (cart.includes(Fruit[id - 1].id)) {
+      setcartItems(cart.filter(item => item !== id))
+
+    }
+    else {
+      setcartItems([...cart, Fruit[id - 1].id]);
+      console.log(cart)
+
+    }
+  }
+
+  const [sum, setBill] = useState(0);
+  const billCalc = () => {
+    let total = 0;
+    cart.forEach(item => {
+      total += Fruit[item - 1].price * Fruit[item - 1].quantity;
+    });
+    setBill(total);
+
+    console.log(sum)
+  };
+  
 
   return (
     <>
-    <div>
-      <h3>Shoping Cart</h3>
+      <div>
+        <h3>Shoping Cart</h3>
 
-      
-          
+
+
         <table>
           <thead>
             <tr>
@@ -40,45 +67,56 @@ const decreaseQuantity = (id) =>{
           </thead>
           <tbody>
             {Fruit.map((fruit) => (
-            <tr key={fruit.id}>
-              <td> {fruit.id} </td>
-              <td> {fruit.name} </td>
-              <td> {fruit.price} </td>
-              <td> <button onClick={() => decreaseQuantity(fruit.id)}>-</button> {fruit.quantity} <button onClick={() => increaseQuantity(fruit.id)}>+</button> </td>
-              <td> <input type="checkbox" name="fruit"/> </td>
-            </tr>
+              <tr key={fruit.id}>
+                <td> {fruit.id} </td>
+                <td> {fruit.name} </td>
+                <td> {fruit.price} </td>
+                <td> <button onClick={() => decreaseQuantity(fruit.id)}>-</button> {fruit.quantity} <button onClick={() => increaseQuantity(fruit.id)}>+</button> </td>
+                <td> <input type="checkbox" name="fruit" onChange={() => handleCartAddOn(fruit.id)} /> </td>
+              </tr>
             ))}
           </tbody>
         </table>
-{/* 
+        {/* 
       {Fruits.map((fruit) =>{
         <input type="checkbox" name="" id="" />
       })} */}
-    </div>
-      
-      
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Item Price</th>
+              <th>Quantity</th>
+              <th>Total Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, i) => (
+              <tr key={i}>
+                <td>{Fruit[item - 1].name}</td>
+                <td>{Fruit[item - 1].price}</td>
+                <td>{Fruit[item - 1].quantity}</td>
+                <td>
+                  {(Fruit[item - 1].price) * (Fruit[item - 1].quantity)}
+                </td>
+
+              </tr>
+
+            )
+            )}
+            <tr>
+              <td><button onClick={() => billCalc()}>Bill</button></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
     </>
-  ) }
+  )
+}
 
 
 export default App
